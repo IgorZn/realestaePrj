a6912c184630e56cbeb7fb7dfd0a4d876a8dfc06
igor.znamensky <igor.znamensky@gmail.com>
2023-06-16 10:23:16 +0300

    next: Media Folder & Adding Data

--- listings/admin.py
+++ listings/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Listing, Realtor
+
+
+@admin.register(Listing)
+class AdminListing(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Realtor)
+class AdminRealtor(admin.ModelAdmin):
+    pass
