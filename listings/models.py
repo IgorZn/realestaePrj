623468f4e9199d5f7cf4cbff0e59b88dfca5b26d
igor.znamensky <igor.znamensky@gmail.com>
2023-06-26 10:26:49 +0300

    next: 7, 6. Search Form Choices

--- listings/models.py
+++ listings/models.py
@@ -18,14 +18,17 @@ class Listing(models.Model):
     sqft = models.IntegerField()
     lot_size = models.DecimalField(max_digits=5, decimal_places=1)
     photo_main = models.ImageField(upload_to='photos/%Y/%m/%d/')
-    photo_1 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
-    photo_2 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
-    photo_3 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
-    photo_4 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
-    photo_5 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
-    photo_6 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
+    photo_1 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True, default='')
+    photo_2 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True, default='')
+    photo_3 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True, default='')
+    photo_4 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True, default='')
+    photo_5 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True, default='')
+    photo_6 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True, default='')
     is_published = models.BooleanField(default=True)
     list_date = models.DateField(default=datetime.now, blank=True)
+
+    def get_photos(self):
+        return self.d
 
     def __str__(self):
         return self.title
