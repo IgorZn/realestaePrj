cb34cb3941231ccc3e18f369800164233e3b86bb
igor.znamensky <igor.znamensky@gmail.com>
2023-07-06 10:19:12 +0300

    next: 8, Logout & Navbar Auth Links

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.models import User
+from django.contrib import auth
 
 
 # Create your views here.
@@ -47,6 +48,20 @@ def register(request):
 
 
 def login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(username=username, password=password)
+
+        if user is not None:
+            auth.login(request, user)
+            messages.add_message(request, messages.SUCCESS, 'You are now logged in')
+            return redirect('dashboard')
+        else:
+            messages.add_message(request, messages.ERROR, 'Invalid credentials')
+            return redirect('login')
+
     return render(request, 'accounts/login.html')
 
 
